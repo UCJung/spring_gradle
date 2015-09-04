814efd281a732d125de3d7edf17d2a1c1155303a
mykumi <mykumi@gmail.com>
2015-09-04 13:19:47 +0900

    Finish Default Spring Java Config - Spring MVC - myBatis & HikariCP - Spring Security using Custom Provider & DB - Properties Load & Encryption

--- src/main/java/com/spay/web/config/SpringSecutiryInitializer.java
+++ src/main/java/com/spay/web/config/SpringSecutiryInitializer.java
@@ -1,6 +1,5 @@
 package com.spay.web.config;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SpringSecutiryInitializer extends AbstractSecurityWebApplicationInitializer {
